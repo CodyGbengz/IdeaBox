488ec0e3e736977224bf6a5ef68cbff6a9e79648
Gbenga <odemichaelgbenga@gmail.com>
2018-02-21 16:54:53 +0100

    chore(setup-webpack): configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ]
+});

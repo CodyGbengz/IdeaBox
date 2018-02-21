0ea92f64a1efa6bd172fad0f354e90694c517b38
Gbenga <odemichaelgbenga@gmail.com>
2018-02-21 07:13:22 +0100

    chore(eslint): configure eslint

--- server/server.js
+++ server/server.js
@@ -1,22 +1,22 @@
 import dotenv from 'dotenv';
 import logger from 'morgan';
 import express from 'express';
-import mongoose from "mongoose";
-import bodyParser from 'body-parser'; 
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
-/** 
+/**
  * express app instance
 */
-const app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 8080;
 // http request logger
 app.use(logger('dev'));
 
-app.get('*', (req, res) => {res.send('here')});
+app.get('*', (req, res) => { res.send('here'); });
 
 // entry point
 app.listen(port);

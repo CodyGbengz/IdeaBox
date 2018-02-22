46528fa56c72846369191ab077f959184191a737
Gbenga <odemichaelgbenga@gmail.com>
2018-02-22 11:32:57 +0100

    chore(routes): create all basic routes

--- server/server.js
+++ server/server.js
@@ -3,18 +3,27 @@ import logger from 'morgan';
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import routes from './routes';
+
 
 /**
  * express app instance
 */
+const { user, idea } = routes;
+const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 8080;
 // http request logger
 app.use(logger('dev'));
+
+/**
+ * API routes
+ */
+app.use(user);
+app.use(idea);
 
 app.get('*', (req, res) => { res.send('here'); });
 

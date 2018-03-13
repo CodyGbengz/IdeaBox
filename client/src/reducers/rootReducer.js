9ed327f2985312b682cfaf72bcb821a5a2aea3bd
Gbenga <odemichaelgbenga@gmail.com>
2018-03-13 12:16:29 +0100

    feat(get-comments): implement fetch idea comments feature

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -2,14 +2,23 @@ import { combineReducers } from 'redux';
 import userReducer from './userReducer';
 import { ideaReducer, singleIdeaReducer } from './ideaReducer';
 import userIdeasReducer from './userIdeasReducer';
+<<<<<<< HEAD
 import filterIdeasReducer from './filterIdeasReducer';
 import searchResult from './searchIdeasReducer';
+=======
+import commentsReducer from './commentsReducer';
+>>>>>>> feat(get-comments): implement fetch idea comments feature
 
 export default combineReducers({
   userReducer,
   ideaReducer,
   userIdeasReducer,
+<<<<<<< HEAD
   filterIdeasReducer,
   searchResult,
   singleIdeaReducer
+=======
+  singleIdeaReducer,
+  commentsReducer
+>>>>>>> feat(get-comments): implement fetch idea comments feature
 });
